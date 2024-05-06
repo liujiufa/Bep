ce04c04854f652a9d7b75c681885b75e3cee5c51
Mr.Li <1752146907@qq.com>
2024-05-06 17:57:28 +0800

    css布局

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,18 +5,22 @@ import MainLayout from "../Layout/MainLayout";
 import DeputyLayout from "../Layout/DeputyLayout";
 
 const Invite = React.lazy(() => import("../view/Invite"));
+const Home = React.lazy(() => import("../view/Home"));
+const Account = React.lazy(() => import("../view/Account"));
 const IDO = React.lazy(() => import("../view/IDO"));
 
 export default function Router() {
   return (
     <Suspense fallback={<PageLoding></PageLoding>}>
       <Routes>
-        <Route path="/*" element={<MainLayout />}>
+        {/* <Route path="/*" element={<MainLayout />}> */}
+        <Route path="/*"  >
           <Route path=":address/">
             <Route index element={<Invite />}></Route>
             <Route path="IDO" element={<IDO />}></Route>
           </Route>
-          <Route path="" element={<Invite />}></Route>
+          <Route path="account" element={<Account />}></Route>
+          <Route path="" element={<Home />}></Route>
         </Route>
         <Route path="/DeputyLayout" element={<DeputyLayout />}></Route>
       </Routes>
